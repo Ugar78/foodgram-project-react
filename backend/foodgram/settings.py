d1e9445766847cd5a7f0e3dc6387e47762efb654
Igor Dolgonosov <garik78@bk.ru>
2024-01-26 22:45:06 +0100

    iter_1.5

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -66,8 +66,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'postgres'),
         'USER': os.getenv('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('POSTGRES_HOST', ''),
         'PORT': os.getenv('POSTGRES_PORT', 5432)
     }
 }
