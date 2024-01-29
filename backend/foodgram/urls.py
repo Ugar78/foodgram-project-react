5ab1f43486da837c1a05a24f3f7c9fab8caf190a
Igor Dolgonosov <garik78@bk.ru>
2024-01-29 14:57:40 +0100

    iter_9.1

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path
 from django.conf import settings
 
 
@@ -10,7 +10,9 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('djoser.urls')),
-    re_path('auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
