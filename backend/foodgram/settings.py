063259290beb09025023997ca13be1bcebd834a8
Igor Dolgonosov <garik78@bk.ru>
2024-01-29 11:14:40 +0100

    iter_8.5

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -103,8 +103,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 AUTH_USER_MODEL = 'users.FoodgramUser'
 
 REST_FRAMEWORK = {
