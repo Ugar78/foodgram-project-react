146c6a2db14973f8ffc360bb446aba186d2728dc
Igor Dolgonosov <garik78@bk.ru>
2024-01-29 12:05:05 +0100

    iter_8.7

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -13,4 +13,4 @@ urlpatterns = [
     re_path('auth/', include('djoser.urls.authtoken')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
